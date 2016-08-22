d8fe28759f5d5c7f40348a85dd7828d4c014c3d2
Philip Schlump <pschlump@gmail.com>
2016-08-22 13:35:20 -0600

    Downloading fiels now

--- index/lib.go
+++ index/lib.go
@@ -1,13 +1,67 @@
 package index
 
+import (
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"regexp"
+	"strings"
+
+	"github.com/pschlump/godebug"
+)
+
+var ErrUnableToGetIndex = errors.New("Unable to get directory index")
+
 // data, err := index.GetDirectory(gCfg.LoadUrl)
 func GetDirectory(URL string) (data []byte, err error) {
+	var status int
+	status, data = HTTPGet(URL)
+	if status != http.StatusOK {
+		err = ErrUnableToGetIndex
+	}
 	return
+}
+
+var matchLine *regexp.Regexp
+
+func init() {
+	matchLine = regexp.MustCompile(`<tr><td><a href="([0-9][0-9]*.zip)">`)
 }
 
 // ParseDirectory takes a directory listing from a file server and extracts the list of file names.
 // TODO: this could be significanly improved using a HTML parser - instead of ``greping'' the file names.
 // that is a 2-4 day project to get it working, however that will significantly improve the reliability of this.
 func ParseDirectory(data []byte) (fns []string, err error) {
+	// <tr><td><a href="1471622300928.zip">1471622300928.zip</a></td><td align="right">19-Aug-2016 19:02  </td><td align="right">9.9M</td><td>&nbsp;</td></tr>
+	lines := strings.Split(string(data), "\n")
+	for _, line := range lines {
+		m := matchLine.FindAllStringSubmatch(line, -1)
+		if len(m) > 0 {
+			fmt.Printf("Matched %s\n", line)
+			if len(m[0]) > 1 {
+				fmt.Printf("Matched Fn=%s\n", m[0][1])
+				fns = append(fns, m[0][1])
+			}
+		}
+	}
+	fmt.Printf("fns =%s\n", godebug.SVarI(fns))
 	return
+}
+
+// TODO: may be better to have a streaming return on this - but ... this is simple for testing.
+func HTTPGet(URL string) (status int, rv []byte) {
+	res, err := http.Get(URL)
+	if err != nil {
+		return 500, []byte("")
+	} else {
+		defer res.Body.Close()
+		var err error
+		rv, err = ioutil.ReadAll(res.Body)
+		if err != nil {
+			return 500, []byte("")
+		}
+		status = res.StatusCode
+		return
+	}
 }
--- main.go
+++ main.go
@@ -10,11 +10,15 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
+	"net/http"
+	"os"
 	"time"
 
 	"github.com/pschlump/Go-FTL/server/sizlib"
+	"github.com/pschlump/radix.v2/redis"
 
 	"www.2c-why.com/nuvi/news-aggregator/index"
 )
@@ -37,6 +41,7 @@ import (
 
 type GlobalConfigType struct {
 	RedisHost                   string          `json:"RedisHost"`                   //
+	RedisPort                   string          `json:"RedisPort"`                   //
 	RedisAuth                   string          `json:"RedisAuth"`                   //
 	RunFreq                     int             `json:"RunFreq"`                     //
 	ServiceName                 string          `json:"ServiceName"`                 //
@@ -46,9 +51,22 @@ type GlobalConfigType struct {
 	RunMode                     string          `json:"RunMode"`                     //
 	DebugFlags                  map[string]bool `json:"DebugFlags"`                  //
 	LoadUrl                     string          `json:"LoadUrl"`                     //
+	TmpDir                      string          `json:"TmpDir"`                      //	Where to create temporary directories
+	TmpPrefix                   string          `json:"TmpPrefix"`                   // Prefix to create the temporary directories with
 }
 
-var gCfg GlobalConfigType
+var gCfg = GlobalConfigType{
+	RedisHost:                   "127.0.0.1",
+	RedisPort:                   "6379",
+	RedisAuth:                   "",
+	RunFreq:                     0,
+	ServiceName:                 "news:aggrigator",
+	RedisPrefix:                 "",
+	RedisKeySetOfFilesDownoaded: "downloaded-files",
+	RedisKeyLoadedDocuments:     "loaded-documents",
+	TmpDir:                      "./tmp",
+	TmpPrefix:                   "na_",
+}
 
 var Rerun = flag.String("rerun", "", "Rerun of a specific .zip file")                  //
 var URL = flag.String("URL", "", "Load from URL - overrides default in cfg.json file") //
@@ -73,21 +91,30 @@ func main() {
 		gCfg.LoadUrl = *URL
 	}
 
+	// connect to Redis
+	client, err := RedisClient(gCfg.RedisHost, gCfg.RedisPort, gCfg.RedisAuth)
+	if err != nil {
+		log.Printf("Unable to connect to Redis, error=%s", err)
+		return
+	}
+
+	os.Mkdir(gCfg.TmpDir, 0700)
+
 	// iterate in a loop if RunFreq > 0, else just run once
 	if gCfg.RunFreq > 0 {
 		for n := 1; ; n++ {
 			fmt.Printf("Running every %d seconds, iteration %d\n", gCfg.RunFreq, n)
-			RunMainProcess()
+			RunMainProcess(client)
 			time.Sleep(time.Duration(gCfg.RunFreq) * time.Second)
 		}
 	} else {
 		fmt.Printf("Running just onece\n")
-		RunMainProcess()
+		RunMainProcess(client)
 	}
 
 }
 
-func RunMainProcess() {
+func RunMainProcess(client *redis.Client) {
 
 	// get list of files -- directory listing via http.Get()
 	data, err := index.GetDirectory(gCfg.LoadUrl)
@@ -103,9 +130,7 @@ func RunMainProcess() {
 		return
 	}
 
-	// xyzzy - connect to Redis
-
-	// xyzzy - remove duplciates for download (if dbOnly1File, then only run 1 file) -- if Rerun - then search for that file
+	// remove duplciates for download (if dbOnly1File, then only run 1 file) -- if Rerun - then search for that file
 	if Rerun != nil && len(*Rerun) > 0 {
 		if sizlib.InArray(*Rerun, fList) {
 			fList = []string{*Rerun}
@@ -114,19 +139,31 @@ func RunMainProcess() {
 			return
 		}
 	}
-	fList = RemoveDuplicateDownloadFiles(fList)
+	fList = RemoveDuplicateDownloadFiles(client, fList)
 	if IsDbOn("dbOnly1File", &gCfg) { // this is for testing - to only run 1 file
 		if len(fList) > 1 {
 			fmt.Printf("Debug flag %s is on, only run 1 file, list reduced from %s to %s\n", "dbOnly1File", fList, fList[0:1])
 			fList = fList[0:1]
 		}
 	}
+	if len(fList) == 0 {
+		fmt.Printf("No new files to process\n")
+		return
+	}
+	fmt.Printf("Processing %s\n", fList)
 
-	// xyzzy - probably need to download into a tmp directory
+	// probably need to download into a tmp directory -- Create the tmp-dir
+	name, err := ioutil.TempDir(gCfg.TmpDir, gCfg.TmpPrefix)
+	if err != nil {
+		log.Printf("Unable to create temporary directory, error=%s", err)
+		return
+	}
+	fmt.Printf("name=%s\n", name)
+
 	// download files form list -- may want to do this in parallel --
-	DownloadZipFiles(fList)
+	DownloadZipFiles(fList, name)
 
-	// xyzzy - create temporary directory for each file to extract into - one temporary for each file - (if db2 then leave directory after run)
+	// skip, just use "name" - create temporary directory for each file to extract into - one temporary for each file - (if db2 then leave directory after run)
 
 	// xyzzy - extract each .zip file - get list of file names. (if db3 then leave .zip file, else if no error then discard)
 
@@ -136,9 +173,13 @@ func RunMainProcess() {
 
 	//		xyzzy - load into list in redis
 
+	// cleanup - remove temporary directories
+	if !IsDbOn("dbLeaveTmpDir", &gCfg) { // this is for testing - leave temporary directory in place
+		os.RemoveAll(name)
+	}
 }
 
-// IsDbOn returns true if a specified debugt flag is enabled.
+// IsDbOn returns true if a specified debug flag is enabled.
 func IsDbOn(flag string, gCfg *GlobalConfigType) (on bool) {
 	// xyzzy lock for debug flags map
 	x, ok := gCfg.DebugFlags[flag]
@@ -159,12 +200,85 @@ func ReadConfigFile(fn string, gCfg *GlobalConfigType) {
 	}
 }
 
-func RemoveDuplicateDownloadFiles(fList []string) (rv []string) {
-	// xyzzy - use Redis set to see if fiel is already down.
-	// xyzzy - possibly check file system for it.
-	rv = fList
+func RemoveDuplicateDownloadFiles(client *redis.Client, fList []string) (rv []string) {
+	// Use Redis set to see if file is already down.
+	for _, fn := range fList {
+		key := gCfg.RedisPrefix + gCfg.RedisKeySetOfFilesDownoaded
+		// TODO: this has a race condition in it - if multiple processes are to be run then this test should be changed to set a key, and check the key in Redis.
+		if !IsInRedisSet(client, fn, key) {
+			rv = append(rv, fn)
+			AddToRedisSet(client, fn, key)
+		}
+	}
 	return
 }
 
-func DownloadZipFiles(fList []string) {
+// download files form list -- may want to do this in parallel --
+func DownloadZipFiles(fList []string, tmpDir string) {
+	for _, fn := range fList {
+
+		fp, err := sizlib.Fopen(tmpDir+"/"+fn, "w")
+		if err != nil {
+			// xyzzy
+		}
+		defer fp.Close()
+
+		URL := gCfg.LoadUrl + "/" + fn
+		HTTPGetToFile(URL, fp)
+	}
+}
+
+func HTTPGetToFile(URL string, fp *os.File) (status int) {
+	res, err := http.Get(URL)
+	if err != nil {
+		// xyzzy - log
+		return
+	}
+	defer res.Body.Close()
+	if res.StatusCode == 200 {
+		_, err := io.Copy(fp, res.Body)
+		if err != nil {
+			// xyzzy - log
+			return
+		}
+	} else {
+		// xyzzy - log - status - why getting non-200 status codes
+	}
+	status = res.StatusCode
+	return
+}
+
+// RedisClient connects to Redis or returns an error.
+func RedisClient(RedisHost, RedisPort, RedisAuth string) (client *redis.Client, err error) {
+	client, err = redis.Dial("tcp", RedisHost+":"+RedisPort)
+	if err != nil {
+		return
+	}
+	if RedisAuth != "" {
+		err = client.Cmd("AUTH", RedisAuth).Err
+		if err != nil {
+			return
+		}
+	}
+	return
+}
+
+func IsInRedisSet(client *redis.Client, fn, key string) bool {
+	// - use sets, SISMEMBER - to find if in set
+	n, err := client.Cmd("SISMEMBER", key, fn).Int()
+	if err != nil {
+		log.Printf("Error: Redis SISMEMBER, %s, %s returned error %s\n", key, fn, err)
+		return false
+	}
+	if n == 1 {
+		return true
+	}
+	return false
+}
+
+func AddToRedisSet(client *redis.Client, fn, key string) {
+	err := client.Cmd("SADD", key, fn).Err
+	if err != nil {
+		log.Printf("Error: Redis SADD, %s, %s returned error %s\n", key, fn, err)
+	}
 }
